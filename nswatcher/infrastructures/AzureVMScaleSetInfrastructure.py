30bff2248dc62dfca5ab10dec442785fa4e58c6b
Paraita Wohler <paraita.wohler@activeeon.com>
2018-07-11 17:23:03 +0200

    Sync with the 8.0.0 version

--- nswatcher/infrastructures/AzureVMScaleSetInfrastructure.py
+++ nswatcher/infrastructures/AzureVMScaleSetInfrastructure.py
@@ -1,12 +1,12 @@
 def build_infra_cmd(infra):
     cmd = f"org.ow2.proactive.resourcemanager.nodesource.infrastructure.AzureVMScaleSetInfrastructure" \
           f" {infra['azureCredentialFile']}" \
-          f" '{infra['customResourceGroupName']}'" \
           f" '{infra['maxVms']}'" \
           f" '{infra['maxNodesPerVM']}'" \
           f" '{infra['machineType']}'" \
           f" '{infra['linuxImage']}'" \
           f" '{infra['sshPublicKey']}'" \
+          f" '{infra['alwaysFulfillVM']}'" \
           f" '{infra['targetNetwork']}'" \
           f" '{infra['publicIp']}'" \
           f" '{infra['backendPortNumber1']}'" \
@@ -14,8 +14,6 @@ def build_infra_cmd(infra):
           f" '{infra['azureRegion']}'" \
           f" '{infra['rmurl']}'" \
           f" '{infra['rmCredentials']}'" \
-          f" '{infra['deploymentTimeout']}'" \
-          f" '{infra['cleaningDelay']}'" \
           f" '{infra['externalStorageAccount']}'" \
           f" '{infra['userCustomStartupScriptUrl']}'" \
           f" '{infra['internalCustomStartupScriptUrl']}'"
